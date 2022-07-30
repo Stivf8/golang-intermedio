3dd9372abf91fe616f92863d25ba4dd0b2c82865
Stiven Herrera <stivenarleyherrera@gmail.com>
2022-07-29 20:04:23 -0500

    se agrega repaso importante

---
+++ repaso.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	var x int
+	x = 8
+	y := 7
+	fmt.Println(x)
+	fmt.Println(y)
+
+	// Capturando valor y error
+	myValue, err := strconv.ParseInt("XD", 0, 64)
+
+	// Validando errores.
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	} else {
+		fmt.Println(myValue)
+	}
+
+	// Mapa clave valor.
+	m := make(map[string]int)
+	m["key"] = 6
+	fmt.Println(m["key"])
+
+	// Slice de enteros.
+	s := []int{1, 2, 3}
+	for index, value := range s {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
+	//agregar valor nuevo al slice se usa append
+	s = append(s, 16)
+	for index, value := range s {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
+}
