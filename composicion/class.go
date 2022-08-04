04ced02f0ed26de774b970b798b4eef3f1277dfa
Stiven Herrera <stivenarleyherrera@gmail.com>
2022-08-03 21:13:24 -0500

    se agrega logica de interfaces

--- composicion/class.go
+++ composicion/class.go
@@ -1,28 +1,57 @@
 package main
 
+//Las interfaces dicen que debe ser implementado mas no dice el como
 import "fmt"
 
-//la herencia en go es la composicion.
-
-type Persona struct {
-	Nombre string
-	Edad   int
+type Person struct {
+	name string
+	age  int
 }
 
-type Empleado struct {
-	Id int
+type Employee struct {
+	id int
 }
 
-//como decimos que el FullTiempoEmpleado tiene propiedades de Persona y Propiedades de Empleado
-type FullTiempoEmpleado struct {
-	Persona
-	Empleado
+type FullTimeEmployee struct {
+	Person
+	Employee
+	endDate string
+}
+
+//funcion que reutiliza funcion getMessage para FullTimeEmployee
+func (ftEmployee FullTimeEmployee) getMessage() string {
+	return "Full Time Employee"
+}
+
+type TemporaryEmployee struct {
+	Person
+	Employee
+	taxRate int
+}
+
+//funcion que reutiliza funcion getMessage para TemporaryEmployee
+func (tEmployee TemporaryEmployee) getMessage() string {
+	return "Temporary Employee"
+}
+
+//definicion interfaz que dice que debe ser implementado getMessage
+type PrintInfo interface {
+	getMessage() string
+}
+
+//se debe definir el como se va a implementar la interfaz para el metodo getMessage
+func getMessage(p PrintInfo) {
+	fmt.Println(p.getMessage())
 }
 
 func main() {
-	ftEmpleado := FullTiempoEmpleado{}
-	ftEmpleado.Nombre = "Stiven"
-	ftEmpleado.Edad = 21
-	ftEmpleado.Id = 1
-	fmt.Println(ftEmpleado)
+	ftEmployee := FullTimeEmployee{}
+	ftEmployee.name = "Name"
+	ftEmployee.age = 2
+	ftEmployee.id = 5
+	fmt.Printf("%v\n", ftEmployee)
+	tEmployee := TemporaryEmployee{}
+	getMessage(tEmployee)
+	getMessage(ftEmployee)
+	//GetMessage(ftEmployee)
 }
